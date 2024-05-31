af8ad1fbf029eb895509b398ed1b642f2840e572
isqua <isqua@isqua.ru>
2024-06-01 04:35:45 +0700

    feat: add stripes to the top and the bottom

--- src/svg.ts
+++ src/svg.ts
@@ -1,16 +1,36 @@
+import { randomInt } from "./utils";
+
 type TextOptions = {
     text: string;
     color: string;
     fontSize: number;
 };
 
+type RectangleOptions = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    color: string;
+};
+
 const lineHeight = 1.2;
+
+const stripeHeight = 12;
+const stripeSpacing = 18;
+const minWidth = 40;
+const maxWidth = 160;
+const marginTop = 18;
+const marginBottom = 602;
+const viewBoxWidth = 1280;
+const minVisibleWidth = 20;
 
 export class SvgBuilder {
     private readonly nameSpace = "http://www.w3.org/2000/svg";
 
     private readonly background: SVGGeometryElement;
     private readonly title: SVGTextElement;
+    private readonly stripes: SVGElement;
 
     constructor(private readonly element: SVGElement) {
         const background =
@@ -29,6 +49,14 @@ export class SvgBuilder {
         }
 
         this.title = title;
+
+        const stripes = this.element.querySelector<SVGElement>(".stripes");
+
+        if (!stripes) {
+            throw new Error("Cannot find stripes element");
+        }
+
+        this.stripes = stripes;
     }
 
     setBackground(color: string) {
@@ -65,5 +93,60 @@ export class SvgBuilder {
         this.title.setAttribute("font-size", `${options.fontSize}px`);
 
         return this;
+    }
+
+    addStripes(colors: string[]) {
+        this.fillStripeSection(marginTop, marginTop + 100, colors);
+        this.fillStripeSection(marginBottom, marginBottom + 100, colors);
+
+        return this;
+    }
+
+    protected fillStripeSection(
+        yStart: number,
+        yEnd: number,
+        colors: string[],
+    ) {
+        let colorIndex = 0;
+        for (let y = yStart; y < yEnd; y += stripeHeight + stripeSpacing) {
+            colorIndex = (colorIndex + 1) % colors.length;
+
+            let x = -maxWidth; // Start from left side, stripes may overflow
+
+            while (x < viewBoxWidth) {
+                const stripeWidth = randomInt(minWidth, maxWidth);
+                const visibleWidth =
+                    stripeWidth + x < 0 ? stripeWidth + x : stripeWidth;
+
+                colorIndex = (colorIndex + 1) % colors.length;
+                const stripeColor = colors[colorIndex];
+
+                if (visibleWidth >= minVisibleWidth) {
+                    this.stripes.appendChild(
+                        this.buildRect({
+                            x,
+                            y,
+                            width: stripeWidth,
+                            height: stripeHeight,
+                            color: stripeColor,
+                        }),
+                    );
+                }
+
+                x += stripeWidth + stripeSpacing;
+            }
+        }
+    }
+
+    protected buildRect({ x, y, width, height, color }: RectangleOptions) {
+        const rect = document.createElementNS(this.nameSpace, "rect");
+
+        rect.setAttribute("x", x.toString());
+        rect.setAttribute("y", y.toString());
+        rect.setAttribute("width", width.toString());
+        rect.setAttribute("height", height.toString());
+        rect.setAttribute("fill", color);
+
+        return rect;
     }
 }
